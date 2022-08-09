83b23fccb03e2db2c6b8ca124b3906e7bfdf4a13
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-09 12:41:05 -0600

    simple JS pseudocoding added

---
+++ main.js
@@ -0,0 +1,12 @@
+// Must have start button initiate the test
+//  Timer must begin when start button is pressed
+
+// A series of questions are asked
+//  Incorrect answers reduce time remaining
+//  Answers are stored and scored for later
+
+// Test ends when all questions are answered OR time runs out
+
+// Score is presented at the end of the game
+//  Initials can be entered to accompany new score
+
