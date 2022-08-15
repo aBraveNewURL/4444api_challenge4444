7f105f22e1ba190b24842eca515a3ab1bd04ee2a
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-14 19:14:41 -0600

    fixed timer, working on form component

--- main.js
+++ main.js
@@ -10,67 +10,99 @@
 // Score is presented at the end of the game
 //  Initials can be entered to accompany new score
 
-let currentQuestion= 0;
-const start= document.getElementById("startBtn");
+
+let currentQuestion = 0;
+const start = document.getElementById("startBtn");
 start.addEventListener("click", beginTest);
-// TIMER function goes here ***
-function beginTest(){
-    start.style.display = "none";
-    testContent.style.display = "block";
-    // document.getElementById()
-    renderQuestion();
-    
+start.addEventListener("click", setTime);
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds remaining.";
+
+        if (secondsLeft <= 0) {
+            // clearInterval(timerInterval);
+
+            highScore();
+        }
+    }, 1000);
 }
 
-const test= document.getElementById("testContent");
-const question= document.getElementById("testQuestion");
-const choiceA= document.getElementById("A");
-const choiceB= document.getElementById("B");
-const choiceC= document.getElementById("C");
-var score= document.getElementById("userScore");
+function beginTest() {
+
+    start.style.display = "none";
+    testContent.style.display = "block";
+    renderQuestion();
+
+    function setTime() {
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft + " seconds remaining.";
+
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                
+                highScore();
+            }
+        }, 1000);
+    }
+}
+
+var timeEl = document.querySelector("h2");
+var secondsLeft = 15
+const test = document.getElementById("testContent");
+const question = document.getElementById("testQuestion");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
 score = 0;
 
-function renderQuestion(){
+function renderQuestion() {
     let q = questions[currentQuestion];
-    question.innerHTML = "<p>" +q.question+ "</p>";
+    question.innerHTML = "<p>" + q.question + "</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
-
-
 }
 
-function checkAnswer(answer){
+function checkAnswer(answer) {
     console.log(answer)
     console.log(questions[currentQuestion].correctChoice === answer)
-    if(questions[currentQuestion].correctChoice === answer){
-        score++; 
+    if (questions[currentQuestion].correctChoice === answer) {
+        score++;
         currentQuestion++;
-        // console.log(score);
-        // console.log(currentQuestion);
-        // console.log(lasQuestion);
-  }
-  else {
-    currentQuestion++;
-  }
+        
+    }
+    else {
+        secondsLeft--;
+        secondsLeft--;
+        secondsLeft--;
+        secondsLeft--;
+        secondsLeft--;
+        currentQuestion++;
+    }
 
-  if(currentQuestion <= lastQuestion){
-    renderQuestion();
-  }
-  else {
-    scoreRender(); 
-  }
+    if (currentQuestion <= lastQuestion) {
+        renderQuestion();
+    }
+    else {
+        scoreRender();
+        highScore();
+        
+    }
 }
 
-function scoreRender(){
+function scoreRender() {
     userScore.style.display = "block";
     testContent.style.display = "none";
-    let scorePercent = Math.round(100 * score /questions.length);
+    timer.style.display = "none";
+    let scorePercent = Math.round(100 * score / questions.length);
     userScore.innerHTML = "You scored: " + scorePercent + "%"
 }
 
 let questions = [
-    
+
     {
         question: "What year was JavaScript first created?",
         choiceA: "1990",
@@ -113,10 +145,32 @@ let questions = [
 
 ];
 
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds remaining.";
+
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            timer.style.display = "none";
+            scoreRender();
+            highScore();
+            form.style.display= "block";
+        }
+    }, 1000);
+}
+
+
+
+
 let lastQuestion = questions.length - 1;
 
 currentQuestion = 0;
+
 renderQuestion()
+
+form.style.display= "none";
 
 
 
